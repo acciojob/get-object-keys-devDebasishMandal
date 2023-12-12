af86797d0db8707600df1eeefff4e4309564a7d7
Debasish Mandal <76836363+devDebasishMandal@users.noreply.github.com>
2023-12-12 23:37:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 //your JS code here. If required.
-let student ={
-	name:"nexus",
+// let student ={
+// 	name:"nexus",
 	
-}
-student.getKeys= function(){
-	let x=[];
-	for(let i in student){
-		x.push(i);
-	}
-	return x;
-}
-console.log(student.getKeys());
+// }
+// student.getKeys= function(){
+// 	let x=[];
+// 	for(let i in student){
+// 		x.push(i);
+// 	}
+// 	return x;
+// }
+// console.log(student.getKeys());
+Object.prototype.getKeys = function () {
+  return Object.keys(this);
+};
